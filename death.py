9472261f4085df23a9a746f94489469f36a5d107
lyudaegorova <74778600+lyudaegorova@users.noreply.github.com>
2021-01-24 14:52:55 +0300

    Update death.py

--- death.py
+++ death.py
@@ -1,5 +1,5 @@
 import pygame
-from screeninfo import get_monitors
+from screeninfo import get_monitors  # Импортируем модуль для получения размера экрана
 
 
 class Death:
@@ -12,10 +12,10 @@ class Death:
     def draw(self):
         pygame.display.set_caption('Supernatural: Team free will')
         size = width, height = int(str(get_monitors()[0]).split('width=')[1][:4]), \
-                               int(str(get_monitors()[0]).split('height=')[1][:4]) - 76
+                               int(str(get_monitors()[0]).split('height=')[1][:4]) - 76 # Применение функции get_monitors
         screen = pygame.display.set_mode(size)
         img = pygame.image.load("fon7.jpg")
-        fon = pygame.transform.scale(img, (width, height))
+        fon = pygame.transform.scale(img, (width, height)) # Настройка размера изображения под экран для фона
         screen.blit(fon, (0, 0))
         font = pygame.font.SysFont('comic sans ms', 50)
 
@@ -24,6 +24,7 @@ class Death:
         txt3 = font.render('Хочешь сыграть еще раз?', True, (255, 255, 255))
         txt4 = font.render('Да', True, (0, 0, 0))
 
+        # Нахождение координат текста, спользуя процентны и найденные длину и высоту экрана и самого текста
         txt_x = width // 2 - txt.get_width() // 2
         txt_x2 = width // 2 - txt2.get_width() // 2
         txt_x3 = width // 2 - txt3.get_width() // 2
@@ -34,6 +35,7 @@ class Death:
         pygame.draw.rect(screen, (128, 128, 128), (txt_x4 - 10, int(height * 0.6),
                                                    txt4.get_width() + 20, txt4.get_height()), 4)
 
+        # Вывод текста на экран
         screen.blit(txt, (txt_x, int(height * 0.1)))
         screen.blit(txt2, (txt_x2, int(height * 0.25)))
         screen.blit(txt3, (txt_x3, int(height * 0.5)))
@@ -53,6 +55,7 @@ class Death:
                     # Координваты границ кнопки
                     a = txt_x4 + txt4.get_width() + 10
                     b = int(height * 0.6) + txt4.get_height()
+                    # Проверка, нажал ли на кнопку пользователь 
                     if int(width * 0.25) - 5 <= x <= a and int(height * 0.4) - 5 <= y <= b:
                         return "restart"
 
